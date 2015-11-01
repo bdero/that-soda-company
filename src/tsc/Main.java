c6d415032ab49b6adf4a5510a9211f466687cd0a
Brandon DeRosier <x@bdero.me>
2015-10-31 22:57:38 -0400

    Setup the camera to be orthographic

--- src/tsc/Main.java
+++ src/tsc/Main.java
@@ -1,9 +1,13 @@
 package tsc;
 
+import com.jme3.app.DebugKeysAppState;
+import com.jme3.app.FlyCamAppState;
 import com.jme3.app.SimpleApplication;
+import com.jme3.app.StatsAppState;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
@@ -19,9 +23,27 @@ public class Main extends SimpleApplication {
         app.start();
     }
 
+    public Main() {
+        super(new StatsAppState(), new FlyCamAppState(), new DebugKeysAppState());
+    }
+
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
+        // Setup the camera to be orthographic and encapsulate a playing field with width 0 to 100, and scale the
+        // height accordingly.
+
+        float fieldWidth = 100;
+        float fieldHeight = fieldWidth*((float)settings.getHeight()/settings.getWidth());
+        float hFieldWidth = fieldWidth/2;
+        float hFieldHeight = fieldHeight/2;
+
+        Camera camera = getCamera();
+        camera.setParallelProjection(true);
+        camera.setFrustum(1, 1000, -hFieldWidth, hFieldWidth, hFieldHeight, -hFieldHeight);
+        camera.update();
+        camera.setLocation(new Vector3f(hFieldWidth, hFieldHeight, 500));
+
+        Box b = new Box(10, 10, 10);
         Geometry geom = new Geometry("Box", b);
 
         Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
