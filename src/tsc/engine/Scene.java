48291bf52d28f8024b874db6e1d56213ee4132d3
Brandon DeRosier <x@bdero.me>
2015-11-02 20:39:06 -0500

    Added engine.Scene

---
+++ src/tsc/engine/Scene.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tsc.engine;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.renderer.Camera;
+
+/**
+ *
+ * @author bdero
+ */
+public class Scene extends AbstractAppState {
+
+    private Application app;
+    private AppStateManager stateManager;
+    private Scene nextScene;
+
+    private boolean transitioning = false;
+    private float screenFade = 1;
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app = app;
+        this.stateManager = stateManager;
+    }
+
+    @Override
+    public void update(float tpf) {
+        Camera camera = app.getCamera();
+
+        super.update(tpf);
+    }
+
+    public void transition(Scene nextScene) {
+        this.nextScene = nextScene;
+        transitioning = true;
+    }
+
+    @Override
+    public void cleanup() {
+        if (nextScene == null) {
+            // Kill the application if no next state was specified.
+            app.stop();
+        } else {
+            // Initialize the next scene
+            stateManager.attach(nextScene);
+        }
+
+        super.cleanup();
+    }
+
+}
